6906df13d962da865f09daa497e9be16689b9778
NoviScl <sichenglei1125@gmail.com>
2019-07-20 19:24:53 +0800

    baseline

--- run_xlnet_dream.py
+++ run_xlnet_dream.py
@@ -399,7 +399,7 @@ def main():
 						help="local_rank for distributed training on gpus")
 	parser.add_argument('--seed',
 						type=int,
-						default=2019,
+						default=42,
 						help="random seed for initialization")
 	parser.add_argument('--gradient_accumulation_steps',
 						type=int,
